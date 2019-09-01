c1776aac1deb9aee02dcfb0dac91df4383c8d4a3
OM KUMAR YADAV <kumarom1203@gmail.com>
2019-09-01 17:37:16 +0530

    this is webhooks concepts

---
+++ server.php
@@ -0,0 +1,32 @@
+<?php
+$data=$_POST;
+$headers = apache_request_headers();
+echo "<pre>";
+print_r($data);
+echo "<hr>";
+
+print_r($headers);
+echo "<hr>";
+$getallheaders  = getallheaders();
+print_r($getallheaders);
+echo "<hr>";
+print_r($_SERVER);
+?>
+
+<pre>
+============ SERVER SIDE CODE server.php =============
+$data=$_POST;
+$headers = apache_request_headers();
+echo "< pre>";
+print_r($data);
+echo "< hr>";
+
+print_r($headers);
+echo "< hr>";
+$getallheaders  = getallheaders();
+print_r($getallheaders);
+echo "< hr>";
+print_r($_SERVER);
+======================================================
+</pre>
+
